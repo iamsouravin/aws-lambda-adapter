5862f376f77fa6f35595f6ee560551763c12e812
EC2 Default User <ec2-user@ip-10-0-141-202.ap-south-1.compute.internal>
2022-04-08 11:32:59 +0000

    Add @ResponseBody for healthz endpoint.

--- examples/springboot/app/src/main/java/com/amazonaws/demo/petstore/Application.java
+++ examples/springboot/app/src/main/java/com/amazonaws/demo/petstore/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -19,6 +20,7 @@ public class Application {
     String message = "";
 
     @RequestMapping(path = "/healthz", method = RequestMethod.GET)
+    @ResponseBody
     public String healthCheck() {
         return "healthy";
     }
