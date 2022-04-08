e4cf61f37cf4cdbd057b4214c0529561dfe515bf
EC2 Default User <ec2-user@ip-10-0-141-202.ap-south-1.compute.internal>
2022-04-08 09:37:53 +0000

    Add frontend.

--- examples/springboot/app/src/main/java/com/amazonaws/demo/petstore/Application.java
+++ examples/springboot/app/src/main/java/com/amazonaws/demo/petstore/Application.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 @Import({ PetsController.class })
 public class Application {
 
@@ -25,7 +25,7 @@ public class Application {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index() {
-        return "Hello, world!";
+        return "index";
     }
 
     public static void main(String[] args) {
